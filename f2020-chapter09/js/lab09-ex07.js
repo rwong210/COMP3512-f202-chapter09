90a9b106e7eec82277cf476063253be0b4467c81
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-18 16:58:49 -0600

    finished ex07

--- f2020-chapter09/js/lab09-ex07.js
+++ f2020-chapter09/js/lab09-ex07.js
@@ -1,12 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
 
    const containers = document.querySelectorAll('.container');
    for (cont of containers) {
-      cont.addEventListener('click', function(e) {
-
-         
+      cont.addEventListener('click', function (e) {
+         console.log(`${e.target.nodeName} was clicked -
+          Propogated to ${e.currentTarget.nodeName}`);
+      }, {
+         capture: true
       });
    }
 
+   const spans = document.querySelectorAll('span');
+   for (s of spans) {
+      s.addEventListener('click', function (e) {
+         console.log('special extra handler for span');
+         e.stopPropagation();
+      })
+   }
 
+   const section = document.querySelector('section');
+   section.addEventListener('click', function (e) {
+      e.stopPropagation();
+   }, {
+      capture: true
+   });
 });
