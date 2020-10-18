2427d1503e8116c9fd6e4855db5d9153606d558b
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-17 18:46:58 -0600

    start ex03

--- f2020-chapter09/js/lab09-ex03.js
+++ f2020-chapter09/js/lab09-ex03.js
@@ -1,2 +1,10 @@
 /* add code after this comment */
 
+//select the <ul>
+const list = document.querySelector("#thumb-list");
+
+//create list item <li>
+const item = document.createElement("img");
+
+//create <img> elementand set its src attribute
+const img
