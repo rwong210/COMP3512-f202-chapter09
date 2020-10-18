6dbe32ee611b700df851188ce4b4c1dbff4ca90c
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-17 18:40:13 -0600

    finished test01

--- f2020-chapter09/js/lab09-test01.js
+++ f2020-chapter09/js/lab09-test01.js
@@ -1,2 +1,12 @@
 /* add code after this comment */
 
+document.getElementById("thumb-list").style.border = "5px solid black";
+
+document.querySelector("textarea").value = document.querySelector("p").textContent;
+
+const images = document.querySelectorAll("ul img");
+
+for (let i of images){
+i.style.boxShadow = "5px 5px rgba(0,0,0,0.5)";
+}
+
