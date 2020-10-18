e0ff0823547f873148271f7a9fed93fbaf49b23d
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-18 13:09:17 -0600

    finished ex04

--- f2020-chapter09/js/lab09-ex04.js
+++ f2020-chapter09/js/lab09-ex04.js
@@ -1,2 +1,8 @@
 /* add code after this comment */
 
+setTimeout( function () {
+    let aside = document.querySelector('aside');
+    aside.classList.toggle('hidden');
+    //aside.classList.add('visible');
+}, 3000);
+
