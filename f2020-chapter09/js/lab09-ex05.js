1e44565c4769616afee3a69bd86e466d6fb86f69
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-18 14:59:18 -0600

    ex05 completed

--- f2020-chapter09/js/lab09-ex05.js
+++ f2020-chapter09/js/lab09-ex05.js
@@ -1,6 +1,36 @@
 /* code goes here */
+document.addEventListener("DOMContentLoaded", function () {
+    function simpleHandler(event) {
+        alert("button was clicked");
+    }
+
+    var btn = document.getElementById("firstButton");
+    btn.addEventListener("click", function () {
+        alert("a different approach but the same result");
+    });
+
+    const buttons = document.querySelectorAll(".card button");
+    for (let btn of buttons) {
+        btn.addEventListener("click", function () {
+            alert("now just one handler");
+        });
+    }
 
 
+    for (let btn of buttons) {
+        btn.addEventListener("click", function (e) {
+            let para = e.target.previousElementSibling;
+            alert(para.textContent);
+        });
+    }
 
-
-
+    const images = document.querySelectorAll(".card img");
+    for (let img of images) {
+        img.addEventListener("mouseover", function (e) {
+            e.target.classList.add("sepia");
+        });
+        img.addEventListener("mouseout", function (e) {
+            e.target.classList.remove('sepia');
+        });
+    }
+});
--- f2020-chapter09/js/lab09-test02.js
+++ f2020-chapter09/js/lab09-test02.js
@@ -1,3 +1,14 @@
 const photos = JSON.parse(content);
 
 /* put your code after this */
+
+const sect = document.querySelector("#parent > section");
+
+for (let p of photos) {
+ const fig = document.createElement("figure");
+ const img = document.createElement("img");
+
+ img.setAttribute("src", `images/${p.filename}`);
+ img.setAttribute("alt", `${p.title}`);
+
+}
