cab31aa0e1cfcb1991d26b1adcb681691abf2534
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-17 20:46:28 -0600

    finished ex4

--- f2020-chapter09/js/lab09-ex03.js
+++ f2020-chapter09/js/lab09-ex03.js
@@ -11,5 +11,8 @@ const img = document.createElement("img");
 img.setAttribute("src", "images/8710320515.jpg");
 
 item.appendChild(img);
-list.appendChild(item);
+//list.appendChild(item);
 
+list.insertAdjacentElement('afterbegin', img);
+
+document.querySelector("#thumb-list li:last-child").remove();
