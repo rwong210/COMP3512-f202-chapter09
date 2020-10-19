767c010b2cd43903ae7b3e9de81714aae9af0331
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-18 20:59:43 -0600

    finished ex09

--- f2020-chapter09/js/lab09-ex09.js
+++ f2020-chapter09/js/lab09-ex09.js
@@ -1,2 +1,28 @@
 /* code goes here */
 
+window.addEventListener("load", function () {
+    document.addEventListener('keypress', playSound);
+    const keys = document.querySelectorAll(".key");
+    for (let key of keys) {
+        key.addEventListener('transitionend', removeTransition)
+    }
+
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+    }
+
+    function playSound(e) {
+        const keyPressed = e.key;
+        const audio = document.querySelector(`audio[data-key="${keyPressed}"]`);
+        audio.currentTime = 0;
+        if (!audio) return;
+        const div = document.querySelector(`div.key[data-key="${keyPressed}"]`);
+        div.classList.add('playing');
+        audio.play();
+    }
+    window.addEventListener('load', function () {
+        document.addEventListener('keypress', playSound);
+
+    })
+});
