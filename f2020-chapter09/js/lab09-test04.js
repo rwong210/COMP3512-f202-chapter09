5f94f58d65b212e1cbe139ab471c9c7d719bbf58
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-19 16:06:53 -0600

    working on test04

--- f2020-chapter09/js/lab09-test04.js
+++ f2020-chapter09/js/lab09-test04.js
@@ -1,2 +1,29 @@
 /* add code after this comment */
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    const thumbNails = document.querySelectorAll("#thumbBox img");
+    //click handler
+    for (let thumbNail of thumbNails) {
+        thumbNail.addEventListener('click', function () {
+            let oldMedium = document.querySelector("figure img");
+            let newMedium = thumbNail.src;
+            newMedium = newMedium.replace("small", "medium");
+            oldMedium.src = newMedium;
+
+            const label = document.querySelector("figcaption");
+            label.innerHTML = `<em>${thumbNail.alt}</em><br><span>${thumbNail.title}</span>`;
+        })
+    }
+
+    const sliders = document.querySelectorAll("input[type=range]");
+        for (let slider of sliders){
+            slider.addEventListener('input', changeSlider);
+            let image = document.querySelector("imgManipulated img");
+            function changeSlider() {
+                image.
+
+            }
+
+        }
+})
