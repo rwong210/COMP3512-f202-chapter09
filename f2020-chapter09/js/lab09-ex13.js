e81cb353e7b29d3e51422077844d24ac27598640
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-19 13:43:50 -0600

    completed ex13

--- f2020-chapter09/js/lab09-ex13.js
+++ f2020-chapter09/js/lab09-ex13.js
@@ -1,5 +1,21 @@
 /* code goes here */
 
+function setBackground(e) {
+    if (e.type == 'focus'){
+        e.target.style.backgroundColor = '#FFE393';
+    }
+    else if(e.type == 'blur'){
+        e.target.style.backgroundColor = 'white';
+    }
+}
 
+document.addEventListener('DOMContentLoaded', function (){
+    const cssSelector = "input[type=text], input[type=password]";
+    const fields = document.querySelectorAll(cssSelector);
 
+    for (let field of fields){
+        field.addEventListener('focus', setBackground);
+        field.addEventListener('blur', setBackground);
+    }
+});
 
