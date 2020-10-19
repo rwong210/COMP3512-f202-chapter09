19967674ecee60326d15d03336022321f5eca975
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-18 20:38:23 -0600

    test03 finished

--- f2020-chapter09/js/lab09-test03.js
+++ f2020-chapter09/js/lab09-test03.js
@@ -1,2 +1,10 @@
 /* add code after this comment */
 
+document.addEventListener('DOMContentLoaded', function () {
+    const panels = document.querySelectorAll(".panel");
+    for (let p of panels) {
+        p.addEventListener('click', function () {
+            p.classList.toggle("open");
+        })
+    }
+});
