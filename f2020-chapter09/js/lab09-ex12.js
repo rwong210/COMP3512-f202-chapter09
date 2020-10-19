7cc60b9eb0badf690413098141dd405c686ddce5
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-19 12:14:46 -0600

    finished w/error ex12

--- f2020-chapter09/js/lab09-ex12.js
+++ f2020-chapter09/js/lab09-ex12.js
@@ -2,6 +2,38 @@
 // see https://codepen.io/imagekit_io/pen/MBNwKB
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
+    const images = document.querySelectorAll("img.lazy");
+
+    document.addEventListener('scroll', lazyload);
+    window.addEventListener('resize', lazyload);
+    window.addEventListenter('orientationChange', lazyload);
+
+    let lazyloadThrottleTimeout = false;
+
+    function lazyload() {
+        if (lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function () {
+            const scrollTop = window.pageYOffset;
+            for (let img of images) {
+                if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.alt = img.dataset.alt;
+
+                    img.classList.remove('lazy');
+                }
+            }
+           /*if (images.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener('resize', lazyload);
+                window.removeEventListener('orientationChange', lazyload);
+            }
+*/
+        }, 20);
+    }
 
 });
