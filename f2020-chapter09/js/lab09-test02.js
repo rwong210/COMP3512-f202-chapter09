77fa700b04a0390e70d6b1a5b0d39e263ae611c7
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-13 14:20:22 -0700

    finished test02 with code formatting

--- f2020-chapter09/js/lab09-test02.js
+++ f2020-chapter09/js/lab09-test02.js
@@ -1,15 +1,51 @@
-
 const photos = JSON.parse(content);
 
 /* put your code after this */
 
-const sect = document.querySelector("#parent > section");
+let section = document.querySelector("section#parent");
 
 for (let p of photos) {
- const fig = document.createElement("figure");
- const img = document.createElement("img");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
 
- img.setAttribute("src", `images/${p.filename}`);
- img.setAttribute("alt", `${p.title}`);
+    img.setAttribute("src", `images/${p.filename}`);
+    img.setAttribute("alt", `${p.title}`);
 
+    figure.appendChild(img);
+
+    // variable to be appended to figure
+    let append = createFigCap(p);
+    
+    figure.appendChild(append);
+    section.appendChild(figure);
+
+    // returns the html markup for each figure element
+    function createFigCap(photo) {
+        // create figcaption element
+        const figCaption = document.createElement("figcaption");
+
+        // create h2 element
+        const h2 = document.createElement("h2");
+        h2.innerText = `${photo.title}`;
+        figCaption.appendChild(h2);
+
+        // create paragraph element
+        const p = document.createElement("p");
+        p.innerText = `${photo.description}`;
+        figCaption.appendChild(p);
+
+        // create colors array
+        const colors = photo.colors;
+
+        // create color spans
+        for (let c of colors) {
+            const span = document.createElement("span");
+            span.style.display = "inline-block";
+            span.innerText = "  ";
+            span.style.backgroundColor = `${c.hex}`;
+            figCaption.appendChild(span);
+        }
+
+        return figCaption;
+    }
 }
