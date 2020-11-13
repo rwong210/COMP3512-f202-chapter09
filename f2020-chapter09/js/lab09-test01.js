0267f0d3de8899c88042adb989e19c3e84176fdf
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-13 12:29:15 -0700

    added padding to ul, finished test01

--- f2020-chapter09/js/lab09-test01.js
+++ f2020-chapter09/js/lab09-test01.js
@@ -1,12 +1,13 @@
 /* add code after this comment */
 
-document.getElementById("thumb-list").style.border = "5px solid black";
+document.getElementById("thumb-list").style.border = "2px solid lightgrey";
 
 document.querySelector("textarea").value = document.querySelector("p").textContent;
 
 const images = document.querySelectorAll("ul img");
 
 for (let i of images){
-i.style.boxShadow = "5px 5px rgba(0,0,0,0.5)";
+i.style.boxShadow = "3px 3px 2.5px rgba(0,0,0,0.5)";
 }
 
+document.querySelector("ul").style.padding = "5px";
